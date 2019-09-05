601919f01558a6dba891b3e18f7f7b7b299a9836
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-09-05 17:39:46 -0400

    understanding overrides

--- src/example.js
+++ src/example.js
@@ -1,12 +1,57 @@
 import * as React from "react";
 import { StatefulCalendar } from "baseui/datepicker";
 
+const arrowBtnOverrides = ({ $theme }) => {
+  return {
+    ":focus": {
+      backgroundColor: $theme.colors.warning500
+    }
+  };
+};
+
+const overrideObj = {
+  CalendarHeader: {
+    style: ({ $theme }) => ({
+      backgroundColor: $theme.colors.warning
+    })
+  },
+  MonthHeader: {
+    style: ({ $theme }) => ({
+      backgroundColor: $theme.colors.warning
+    })
+  },
+  MonthYearSelectButton: {
+    style: ({ $theme }) => ({
+      ":focus": {
+        backgroundColor: $theme.colors.warning500,
+        outline: "none"
+      }
+    })
+  },
+  PrevButton: {
+    style: arrowBtnOverrides
+  },
+  NextButton: {
+    style: arrowBtnOverrides
+  },
+  Day: {
+    style: ({ $theme, $selected, $isHovered, $isHighlighted }) => ({
+      backgroundColor: $selected
+        ? $theme.colors.warning
+        : $isHovered || $isHighlighted
+        ? $theme.colors.warning100
+        : "transparent"
+    })
+  }
+};
+
 export default () => {
   return (
     <StatefulCalendar
       // use the 'onChange' prop to pull data from the component into your application state
       onChange={({ date }) => console.log(date)}
       initialState={{ value: new Date() }}
+      overrides={overrideObj}
     />
   );
 };
